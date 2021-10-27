7116ae5a3267e796e59457aa3ba15d65c5a7cabc
Greig Paais <greigpaais@gmail.com>
2021-10-27 17:40:55 +0700

    Prepare/Create MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-app.listen(8800,()=>{
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,()=>{
+    console.log("Connected to MongoDB")
+});
+
+app.listen(8800, () => {
     console.log("Backend server is running!")
 })
