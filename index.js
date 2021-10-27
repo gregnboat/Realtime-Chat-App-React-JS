7c86dd292f077958e178dab549ff2948a3bbeae1
Greig Paais <greigpaais@gmail.com>
2021-10-27 18:36:58 +0700

    Using Middleware and First Request

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ mongoose.connect(process.env.MONGO_URL,()=>{
     console.log("Connected to MongoDB")
 });
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to homepage!")
+})
+
 app.listen(8800, () => {
     console.log("Backend server is running!")
 })
